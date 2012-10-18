3a217eacc30718275c02201b071cd52fb30b8f99
vishwanth <vishwa.hyd@gmail.com>
2012-10-18 23:00:13 +0530

    Trying to learn networkx

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 from scipy import *
 from scipy.linalg import *
 from scipy.ndimage import *
+from numpy.fft import fft2, ifft2, fftshift
+from scipy.signal import convolve2d as conv
+import matplotlib.pyplot as plt
+import networkx as nx
 import Image
 
 '''
@@ -28,3 +32,77 @@ sure that we will have X nodes which have high centrality, we could as well
 do graph partitioning.
 
 '''
+PATCH_SIZE = 8
+
+im = imread('template.jpg')
+imR = im[:,:,0]
+imG = im[:,:,1]
+imB = im[:,:,2]
+
+x, y = imR.shape
+residX = x % PATCH_SIZE
+residY = y % PATCH_SIZE
+
+xdim = x - residX
+ydim = y - residY
+
+# As of now, we will just adjust the image brutely so that dimensions are
+# multiples of patch size
+imR = imR[:xdim, :ydim]
+imG = imG[:xdim, :ydim]
+imB = imB[:xdim, :ydim]
+
+# We now need to create our feature vector. We need to iterate through the
+# whole image and extract patches. Tiresome job!
+
+iterx = xdim / PATCH_SIZE
+itery = ydim / PATCH_SIZE
+fvector = []
+for i in range(iterx):
+    for j in range(itery):
+        x1 = i * PATCH_SIZE
+        y1 = j * PATCH_SIZE
+        x2 = x1 + PATCH_SIZE
+        y2 = y1 + PATCH_SIZE
+
+        imchunkR = imR[x1:x2, y1:y2]
+        imchunkG = imG[x1:x2, y1:y2]
+        imchunkB = imB[x1:x2, y1:y2]
+
+        # Our first features will be average R, G, B values
+        Rval = sum(imchunkR) / (PATCH_SIZE*PATCH_SIZE)
+        Gval = sum(imchunkG) / (PATCH_SIZE*PATCH_SIZE)
+        Bval = sum(imchunkB) / (PATCH_SIZE*PATCH_SIZE)
+
+        # Next feature should be the position of the patch
+        xpos = (x1 + x2)/2
+        ypos = (y1 + y2)/2
+
+        # We can take a fourier transform and find the maximum frequency.
+        # This would tell us how non uniform is our image.
+
+        IMR = abs(fft2(imchunkR))
+        IMG = abs(fft2(imchunkG))
+        IMB = abs(fft2(imchunkB))
+
+        FXR, FYR = where(IMR == IMR.max())
+        FXG, FYG = where(IMG == IMG.max())
+        FXB, FYB = where(IMB == IMB.max())
+
+        # Spacial comparision will connect nearby neighbours. However,
+        # frequency content comparision may connect two plane areas. Not sure
+        # how to avoid that thing. Anyways, I am not sure that would matter,
+        # since our weights should take care of that.
+
+        # As of now, create the feature vector.
+        
+        fvector.append([[Rval, Gval, Bval], [xpos, ypos],
+                        [[FXR, FYR], [FXG, FYG], [FXB, FYB]]])
+
+node_count = len(fvector)
+imgraph = nx.Graph()
+for i in range(node_count):
+    node_name = 'node_'+str(i)
+    Cval, pos, FFT = fvector[i]
+    imgraph.add_node(i, Cval=Cval, pos=pos, FFT=FFT)
+    
