cf6ff24d2aa5abe7a08e2476456b1df226fa5cfa
vishwanth <vishwa.hyd@gmail.com>
2012-11-09 21:31:00 +0530

    Added a canny script. Testing combining images

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 from generator import *
 from processor import *
-
-patch_size = 10
-im = imread('new_image.jpg')
-G = create_graph(im,patch_size)
+from canny import Canny
+patch_size = 8
+imname = 'flower.jpg'
+im = imread(imname)
+im_edge = Canny(imname, 1).grad[1:-1, 1:-1]
+im_high = empty_like(im)
+im_high[:,:,0] = im_edge[:,:]
+im_high[:,:,1] = im_edge[:,:]
+im_high[:,:,2] = im_edge[:,:]
+G = create_graph(gaussian_filter(im, 2)+im_high,patch_size)
 community = process_graph(G)
 save_community_snapshot(im, G, community, patch_size)
