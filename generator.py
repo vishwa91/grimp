e9b5ec2217d6973ba4a732e5aa7d4a0fe825403e
vishwanth <vishwa.hyd@gmail.com>
2012-11-13 00:24:27 +0530

    Planar graph is good. Tried removing multiple entropies. Need to conclude on that.

--- generator.py
+++ generator.py
@@ -1,12 +1,87 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 
 from scipy import *
 from scipy.linalg import *
 from scipy.ndimage import *
 from scipy.signal import convolve2d as conv
 from scipy.special import *
+from numpy.fft import fft, fft2, fftshift
 
 import networkx as nx
+
+def vader_entropy_function(pixel_group, nbins=18):
+    num_pixels = pixel_group.shape[0]
+    # Find the angles of each point in degrees
+    x = arange(-num_pixels/2, num_pixels/2)
+    grid = meshgrid(x, x)
+    angles = angle(grid[0] + 1j*grid[1], deg=True)
+    # Create histrogram buckets and find indices of the spectrum array that
+    # fall into a particular bucket
+    NUM_HISTOGRAM_BUCKETS = nbins
+    diff = -360/NUM_HISTOGRAM_BUCKETS
+    buckets = arange(180, -180+diff, diff)
+    indices = {}
+    for i in range(0, NUM_HISTOGRAM_BUCKETS):
+        indices[i] = where((angles <= buckets[i]) * 
+                              (angles > buckets[i+1]) )
+    buckets = buckets[:-1]
+    # First calculate the centre-shifted fourier transform of the pixel
+    # group, and then apply a log transformation to get the magnitude
+    # spectrum
+    transformed_pixel_group = fft2(pixel_group)
+    centre_shifted_pixel_group = fftshift(transformed_pixel_group)
+    fourier_spectrum = log(abs(centre_shifted_pixel_group) + 1)
+    
+    # Calculate the orientation histogram of the log magnitude spectrum
+    # by summing over groups of angles. The histogram value at a given
+    # angle should give the power in the log magnitude spectrum around that
+    # angle (approximately)
+    histogram = empty(buckets.shape)
+    for j in range(NUM_HISTOGRAM_BUCKETS):
+        histogram[j] = fourier_spectrum[indices[j]].sum()
+    
+    # Finally, calculate the orientation entropy based on the standard
+    # statistical formula:
+    #       E = H(θ) * log(H(θ))
+    if not histogram.all():
+        entropy = 0
+    else:
+        entropy = - (histogram * log(histogram)).sum()
+    if isnan(entropy):
+        print histogram
+        print fourier_spectrum
+        sys.exit(1)
+    return -1*entropy
+
+def vishwa_entropy_function(patch, nbins=18):
+    # This method is based on the article given in wikipedia.
+    kernx = array([-1, 0, 1,
+                   -1, 0, 1,
+                   -1, 0, 1]).reshape(3,3)
+    kerny = array([-1, -1, -1,
+                    0,  0,  0,
+                    1,  1,  1]).reshape(3,3)
+    imx = conv(patch, kernx)
+    imy = conv(patch, kerny)
+    grad = 180 + 180*arctan2(imy, imx)/pi
+
+    # Segregate the orientations into bins
+    bin_len_l = 0
+    bin_len_h = 0
+    hist = []
+    for k in range(nbins):
+        bin_len_l = bin_len_h
+        bin_len_h += 360.0 / nbins
+        x, y = where((patch <= bin_len_h)*(patch > bin_len_l))
+        hist.append(len(x))
+    H = array(hist)
+    x = where(H == 0)       # This is to avoid log(0)
+    H[x] = 1
+
+    # Calculate entropy using E = H * log(H)
+    E = sum(H * log(H))
+    return E
 
 def _preprocess_image(im, patch_size=8):
     '''
@@ -87,7 +162,7 @@ def _create_feature_vector(im, patch_size = 8):
             fvector.append([Cb, Cr, pos])
             # We now need to find Entropy at 5 scales.
             scale_vector = [0.5, 1.0, 1.5, 2.0, 2.5]
-            
+            '''
             for var in scale_vector:                
                 # Calculate the orientation histogram
                 
@@ -104,15 +179,20 @@ def _create_feature_vector(im, patch_size = 8):
                 for k in range(nbins):
                     bin_len_l = bin_len_h
                     bin_len_h += 360.0 / nbins
-                    x, y = where((imchunk <= bin_len_h)*(imchunk > bin_len_l))
+                    x, y = where((imchunkY <= bin_len_h)*(imchunkY > bin_len_l))
                     hist.append(len(x))
                 H = array(hist)
                 x = where(H == 0)       # This is to avoid log(0)
                 H[x] = 1
 
                 # Calculate the entropy.
-                E = sum(H * log(H))
+                #E = sum(H * log(H))
+                # Calculate Entropy using vader's function.
+                E = vader_entropy_function(imchunkY)
                 fvector[-1].append(E)
+            '''
+            E = vader_entropy_function(imchunkY)
+            fvector[-1].append(E)
     return fvector
 
 def _create_graph(fvector, xdim, ydim, patch_size):
@@ -123,9 +203,11 @@ def _create_graph(fvector, xdim, ydim, patch_size):
     node_count = len(fvector)
     G = nx.Graph()
     for i in range(node_count):
-        Cb, Cr, pos, e1, e2, e3, e4, e5 = fvector[i]
-        G.add_node(i, Cb=Cb, Cr=Cr, pos=pos, e1=e1,
-                   e2=e2,e3=e3,e4=e4,e5=e5)
+        #Cb, Cr, pos, e1, e2, e3, e4, e5 = fvector[i]
+        #G.add_node(i, Cb=Cb, Cr=Cr, pos=pos, e1=e1,
+        #           e2=e2,e3=e3,e4=e4,e5=e5)
+        Cb, Cr, pos, e = fvector[i]
+        G.add_node(i, Cb = Cb, Cr = Cr, pos = pos, e = e)
         # To save time, create edges simultaneously.
 
         if i == 0:
@@ -136,15 +218,16 @@ def _create_graph(fvector, xdim, ydim, patch_size):
                 Cb0 = node['Cb']
                 Cr0 = node['Cr']
                 pos0 = node['pos']
-                e10 = node['e1']
-                e20 = node['e2']
-                e30 = node['e3']
-                e40 = node['e4']
-                e50 = node['e5']
+                e1 = node['e']
+                #e20 = node['e2']
+                #e30 = node['e3']
+                #e40 = node['e4']
+                #e50 = node['e5']
 
                 # Create the differences vector
-                V = array([Cb0-Cb, Cr0-Cr, e1-e10,
-                           e2-e20, e3-e30, e4-e40, e5-e50])
+                #V = array([Cb0-Cb, Cr0-Cr, e1-e10,
+                #           e2-e20, e3-e30, e4-e40, e5-e50])
+                V = array([Cb0-Cb, Cr0-Cr, e1-e])
                 dist_max = hypot(xdim, ydim) * 1.0
                 dist = hypot(pos[0]-pos0[0], pos[1]-pos0[1])
 
@@ -152,7 +235,7 @@ def _create_graph(fvector, xdim, ydim, patch_size):
                 #weight = exp(-dot(V, V.T)) * (1 - (dist/dist_max))
                 weight = exp(-dot(V, V.T))
                 #if weight > 10e-30:
-                if dist < patch_size * sqrt(2.1):
+                if (dist < patch_size * sqrt(2.1)) and (weight > 10e-30):
                     G.add_edge(i, count, weight=weight)
                 count += 1
     return G
@@ -168,3 +251,4 @@ def create_graph(im, patch_size=8):
     return _create_graph(fvector, x, y, patch_size)
 if __name__ == '__main__':
     im = imread('src/ball1.jpg')
+    t = im[:,:,0]
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from processor import *
 import metis
 
 patch_size = 8
-imname = 'src/flower.jpg'
+imname = 'src/new_image.jpg'
 im = imread(imname)
 imtemp = im.copy()
 print 'Starting graph processing'
@@ -14,11 +14,13 @@ comm, partition = process_graph(G)
 save_community_snapshot(im, G, comm, patch_size)
 node_list = [d for n,d in G.nodes_iter(data=True)]
 print 'Level Zero done.'
-(edgecuts, parts) = metis.part_graph(G, nparts=3)
-
-for i in range(len(parts)):
-    if parts[i]==1:
-        x, y = G.node[i]['pos']
-        t = patch_size // 2
-        imtemp[x-t:x+t, y-t:y+t] += 50
-Image.fromarray(imtemp).convert('RGB').save('partition_check.jpg')
+(edgecuts, parts) = metis.part_graph(G, nparts=2)
+nparts = max(parts)
+for p in range(nparts+1):
+    imout = imtemp.copy()
+    for i in range(len(parts)):
+        if parts[i]==p:
+            x, y = G.node[i]['pos']
+            t = patch_size // 2
+            imout[x-t:x+t, y-t:y+t] += 50
+    Image.fromarray(imout).convert('RGB').save('partition_check'+str(p)+'.jpg')
